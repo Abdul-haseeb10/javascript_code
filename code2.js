d3fb58a36140e42f3193e46fe7bbaa5811c7099f
Abdul Haseeb <abdulhaseeb3780@gmail.com>
2023-10-10 23:35:47 +0500

    first

---
+++ code2.js
@@ -0,0 +1,13 @@
+// Variable declaration
+let myNumber = 42;
+
+// Function definition
+function doubleNumber(number) {
+  return number * 2;
+}
+
+   // Using the function
+let result = doubleNumber(3);
+
+// Logging the result
+console.log(result);
