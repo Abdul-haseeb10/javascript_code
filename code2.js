1e5d7a5ab8243c06cffb5ab749b630f391428782
Abdul Haseeb <abdulhaseeb3780@gmail.com>
2023-10-11 21:11:38 +0500

    changes in file

--- code2.js
+++ code2.js
@@ -11,3 +11,86 @@ let result = doubleNumber(3);
 
 // Logging the result
 console.log(result);
+
+//there are three ways to declare a  varible
+//let & const
+/*var a;
+var b = "3"
+a = "7"
+
+b =  "a"
+console.log(a)
+ var studlyCapVar;
+ var upperCase;
+var titleCaseBAR;
+
+
+//ASSIGNMENTS
+studlyCapVar = 10;
+upperCase = "a string"
+titleCaseBar = 100;
+console.log(upperCase)
+           //un initialize  variables
+var a = 3;
+var b = 4;
+var c ="iam a";
+
+//
+a = a + 5
+b = b +4
+c = " string!"
+console.log(a);
+console.log(c);
+            // adding numbers*************
+var a;
+var b;
+sum = (a + b)
+ a = 7;
+ b = 8;
+ console.log(sum)
+            //double string *********************
+var myStr = "firstline\n\t\\second line\nthirdline";
+console.log(myStr)
+        // cancatinating string**************************************
+//var myData = "this is start. " + " this is end"
+//console.log(myData)
+//*****************************bracket notation in  string  brackets
+var myFirstNameLatter = "";
+var myName = "abdul"
+
+myFirstNameLatter = myName[0]
+console.log(myFirstNameLatter)
+//******************* 
+
+var firstName = " ";
+var myName = "abdul"
+ 
+var firstName = myName[myName.length - 3]
+console.log(firstName)
+**************************words blanks************
+
+function wordBlanks(name, place, pet) {
+    var result = "My name is " + name + ", my place is " + place + ", and my pet is a " + pet + ".";
+    return result;
+}
+
+console.log(wordBlanks("Abdul Haseeb", "Islamabad", "cat"));
+
+****************Arrays******************
+var ourArray = [30,20,10]
+var myArary = [];
+
+myArary=ourArray[2]
+console.log(myArary)
+// filter  eklements  in  function having an array
+
+function filterEvenNumbers(arr) {
+  return arr.filter(function (element) {
+      return element % 2 === 0;
+  });
+}
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var evenNumbers = filterEvenNumbers(numbers);
+console.log(evenNumbers);*/
+
